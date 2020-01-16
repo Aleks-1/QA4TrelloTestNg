32ba5009c281fe0a8fd771b46da5bbd91d699378
Aleks-1 <aleks.goldman79@gmail.com>
2020-01-16 19:19:58 +0200

    ver 3.2 Update with FindBy

--- src/tel_ran/tests/LoginPageTests.java
+++ src/tel_ran/tests/LoginPageTests.java
@@ -1,13 +1,7 @@
 package tel_ran.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import tel_ran.helpers.BoardsPageHelper;
@@ -21,9 +15,9 @@ public class LoginPageTests extends TestBase{
 
     @BeforeMethod
     public void initTests(){
-        homePage = new HomePageHelper(driver);
-        loginPage = new LoginPageHelper(driver);
-        boardsPage = new BoardsPageHelper(driver);
+        homePage = PageFactory.initElements(driver,HomePageHelper.class);
+        loginPage = PageFactory.initElements(driver,LoginPageHelper.class);
+        boardsPage = PageFactory.initElements(driver,BoardsPageHelper.class);
     }
 
 
